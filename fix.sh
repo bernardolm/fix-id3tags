39d5e0be9e44852fc3b07eb4bca69e9646e54ce9
Bernardo Loureiro <bernardo.lou@gmail.com>
2017-10-24 00:51:18 -0200

    Updating pub date

--- fix.sh
+++ fix.sh
@@ -4,10 +4,15 @@ MAX=$(cat atom.xml| grep '<item>' -c)
 
 FILE_PREFIX="http://media.grokpodcast.com/"
 
-for i in `seq 1 $MAX`
+for i in `seq 148 $MAX`
 do
     TITLE=$(eval "xmllint --xpath 'string(//channel/item[$i]/title)' atom.xml")
     echo "TITLE = $TITLE"
+
+    DATE=$(eval "xmllint --xpath 'string(//channel/item[$i]/pubDate)' atom.xml")
+    echo "DATE = $DATE"
+    DATE=$(date -d "$DATE" +%Y-%m-%d)
+    echo "DATE = $DATE"
 
     IMG_URL=$(eval "xmllint --xpath \"string(//channel/item[$i]/*[local-name()='image']/@href)\" atom.xml")
     echo "IMG_URL = $IMG_URL"
@@ -26,7 +31,7 @@ do
     echo "FILE_PATH = $FILE_PATH"
 
     mid3v2 --convert --verbose "$(echo $FILE_PATH)"
-    mid3v2 --artist="Grok Podcast" --album="Grok Podcast" --song="$(echo $TITLE)" --genre="Podcast" --track="$(echo $i)" --verbose --picture="$(echo $IMG_PATH)" "$(echo $FILE_PATH)"
+    mid3v2 --artist="Grok Podcast" --album="Grok Podcast" --song="$(echo $TITLE)" --genre="Podcast" --track="$(echo $i)" --verbose --picture="$(echo $IMG_PATH)" --date="$(echo $DATE)" "$(echo $FILE_PATH)"
     mid3iconv --debug $(echo "media/$FILE")
 
     echo -e "\n---\n"
