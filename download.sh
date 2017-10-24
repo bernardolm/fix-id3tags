272754793e87e7b0a14a89239f7169ac29e34a8a
Bernardo Loureiro <bernardo.lou@gmail.com>
2017-10-23 23:20:41 -0200

    Downloading atom file

--- download.sh
+++ download.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-URLS=$(wget http://www.grokpodcast.com/atom.xml -q -O - | grep enclosure | grep --only-matching --perl-regexp "https?\:\/\/[A-Za-z0-9-/.?=_]*")
+wget http://www.grokpodcast.com/atom.xml -q
+
+URLS=$(cat atom.xml | grep enclosure | grep --only-matching --perl-regexp "https?\:\/\/[A-Za-z0-9-/.?=_]*")
 
 max=$(echo $URLS | grep -c http)
 
