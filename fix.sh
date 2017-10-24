8958b1fbee305ca81a9d7d4d13a8f6eee7af3b4f
Bernardo Loureiro <bernardo.lou@gmail.com>
2017-10-24 00:43:34 -0200

    Updating image

--- fix.sh
+++ fix.sh
@@ -6,17 +6,19 @@ FILE_PREFIX="http://media.grokpodcast.com/"
 
 for i in `seq 1 $MAX`
 do
-    TITLE_COMMAND="xmllint --xpath 'string(//channel/item["$i"]/title)' atom.xml"
-    TITLE=$(eval $TITLE_COMMAND)
+    TITLE=$(eval "xmllint --xpath 'string(//channel/item[$i]/title)' atom.xml")
     echo "TITLE = $TITLE"
 
-    URL_COMMAND="xmllint --xpath 'string(//channel/item["$i"]/enclosure/@url)' atom.xml"
-    URL=$(eval $URL_COMMAND)
+    IMG_URL=$(eval "xmllint --xpath \"string(//channel/item[$i]/*[local-name()='image']/@href)\" atom.xml")
+    echo "IMG_URL = $IMG_URL"
+    IMG_PATH="media/$(basename "$IMG_URL")"
+    wget $IMG_URL --continue -q -P media/ -O $IMG_PATH
+
+    URL=$(eval "xmllint --xpath 'string(//channel/item[$i]/enclosure/@url)' atom.xml")
     echo "URL = $URL"
     wget $URL --continue -q -P media/
 
-    FILE_COMMAND="xmllint --xpath 'string(//channel/item["$i"]/guid)' atom.xml"
-    FILE=$(eval $FILE_COMMAND)
+    FILE=$(eval "xmllint --xpath 'string(//channel/item[$i]/guid)' atom.xml")
     FILE=${FILE#$FILE_PREFIX}
     echo "FILE = $FILE"
 
@@ -24,7 +26,7 @@ do
     echo "FILE_PATH = $FILE_PATH"
 
     mid3v2 --convert --verbose "$(echo $FILE_PATH)"
-    mid3v2 --artist="Grok Podcast" --album="Grok Podcast" --song="$(echo $TITLE)" --genre="Podcast" --track="$(echo $i)" --verbose "$(echo $FILE_PATH)"
+    mid3v2 --artist="Grok Podcast" --album="Grok Podcast" --song="$(echo $TITLE)" --genre="Podcast" --track="$(echo $i)" --verbose --picture="$(echo $IMG_PATH)" "$(echo $FILE_PATH)"
     mid3iconv --debug $(echo "media/$FILE")
 
     echo -e "\n---\n"
